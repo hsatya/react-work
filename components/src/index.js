8fb924dff3cf79d04550be0b3aee31b218468d7b
User <harshit.satya@perpetualblock.io>
2022-11-03 00:06:34 +0530

    components

--- components/src/index.js
+++ components/src/index.js
@@ -4,9 +4,17 @@ import { faker } from "@faker-js/faker";
 import CommentDetail from "./CommentDetail";
 import ApprovalCard from "./ApprovalCard";
 
+import "./style/App.css";
+
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
       <ApprovalCard>
         <CommentDetail
           author="Sam"
